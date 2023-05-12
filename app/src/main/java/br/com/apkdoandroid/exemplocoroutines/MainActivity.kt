538bd7a2fa75f615be46fb5857600502bb0f80bf
Elanilson de Jesus <elanilsonpp@hotmail.com>
2023-05-12 15:59:27 -0300

    tarefas simutateas, tempo, para execucao

--- app/src/main/java/br/com/apkdoandroid/exemplocoroutines/MainActivity.kt
+++ app/src/main/java/br/com/apkdoandroid/exemplocoroutines/MainActivity.kt
@@ -7,12 +7,16 @@ import android.util.Log
 import br.com.apkdoandroid.exemplocoroutines.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
+import kotlin.system.measureNanoTime
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private var job : Job? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -32,11 +36,83 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        binding.buttonParar.setOnClickListener {
+            job?.cancel()
+            binding.buttonSuspensa.text = "Reiniciar"
+            binding.buttonSuspensa.isEnabled = true
+        }
+
         binding.buttonSuspensa.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
-                executar()
+              //  executar()
+                job =  CoroutineScope(Dispatchers.IO).launch {
+                   /* withTimeout(5000L){
+                        exec()
+                    }*/
+
+                    //tarefas simultaneas
+
+                    var tempo  =  measureNanoTime {
+                        var resultado1: String? = null
+                        var resultado2: String? = null
+                      val job1 =  launch {
+                            resultado1 = tarefa1()
+                        }
+                      val job2 =  launch {
+                            resultado2 = tarefa2()
+                        }
+                        // val resultado1 = tarefa1()
+                        // val resultado2 = tarefa2()
+
+                        job1.join()
+                        job2.join()
+                        Log.d("Coroutines","Resultado1: $resultado1")
+                        Log.d("Coroutines","Resultado2: $resultado2")
+                    }
+                    Log.d("Coroutines","Tempo: $tempo")
+
+                }
+
             }
         }
+
+    }
+
+    private suspend fun  tarefa1() : String{
+        repeat(15){index ->
+            Log.d("Log.d(\"Coroutines\",\"tarefa2: $index - Thread: ${Thread.currentThread().name}\")","tarefa1: $index - Thread: ${Thread.currentThread().name}")
+            //Thread.sleep(2000)
+            delay(2000)
+
+        }
+        return "Executou tarefa 1"
+    }
+
+    private suspend fun  tarefa2() : String{
+        repeat(15){index ->
+            Log.d("Coroutines","tarefa2: $index - Thread: ${Thread.currentThread().name}")
+            //Thread.sleep(2000)
+            delay(2000)
+
+        }
+        return "Executou tarefa 2"
+    }
+
+    private suspend fun  exec(){
+
+
+            repeat(15){index ->
+                Log.d("Coroutines","Contando: $index - Thread: ${Thread.currentThread().name}")
+                //Thread.sleep(2000)
+                delay(2000)
+                withContext(Dispatchers.Main){
+                    binding.buttonSuspensa.text = "Contando: $index - Thread: ${Thread.currentThread().name}"
+                    binding.buttonSuspensa.isEnabled = false
+
+                }
+            }
+
+
 
     }
 
